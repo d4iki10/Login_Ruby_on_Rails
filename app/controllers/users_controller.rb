44300e06efa53ca7970b9868bd154788e87f45dd
Daiki <daqidiye@gmail.com>
2024-10-02 10:54:04 +0900

    first_commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # ユーザ登録に成功した場合の処理
+      log_in(@user)
+      flash[:notice] = t('flash.users.create') 
+      redirect_to user_path(@user.id)
+    else
+      # ユーザ登録に失敗した場合の処理
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = t('flash.users.update') 
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy 
+    flash[:notice] = t('flash.users.destroy') 
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to current_user unless current_user?(@user)
+  end
+end
