8bb2043daec7f77019d46b23a669d7edf9e99260
Daiki <daqidiye@gmail.com>
2024-10-02 11:27:19 +0900

    second_commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ログイン成功した場合
+      flash[:notice] = 'ログインしました'
       log_in(user)
       redirect_to user_path(user.id)
     else
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+  skip_before_action :login_required
+  
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
